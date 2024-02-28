cf19bbcf625fa1c6d725c9729f3d51d7d2344e08
bsowmiya=567 <sowzhasai25@gmail.com>
2024-02-28 18:44:07 +0530

    2nd commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(cors({origin: 'http://localhost:3000'}))
 app.use(express.json())
 app.use('/',userRouter)
-app.get('/',()=>{
+app.get('/',(req,res)=>{
     res.status(200).json({message:"Welcome"})
 })
 connectDB()
